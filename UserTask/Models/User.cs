b6022830be6dee2607e1b6756d43edbea012281c
dbBurzum <khalidsr@code.edu.az>
2022-03-22 23:20:47 +0400

    Add project files.

---
+++ UserTask/Models/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_AccesModifiers_Encapsulation_Readonly.Models
+{
+    internal class User
+    {
+        private string _username;
+        private int _age;
+        private string _password;
+
+
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value.Length < 5 || value.Length > 10)
+                {
+                    Console.WriteLine($"Username can not be out of range 5-10");
+                }
+                else
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+
+
+                _password = value;
+
+
+
+
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    Console.WriteLine("Age can not be out of range 0-120");
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+        public void UserInfo()
+        {
+            Console.WriteLine($"You are now named {Username} as a user \n" +
+                $"Your age is set to  {Age} \n" +
+                $"Your password is set to {Password}");
+        }
+    }
+}
---
+++ UserTask/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Reflection;
+using CSharp_AccesModifiers_Encapsulation_Readonly.Models;
+
+namespace CSharp_AccesModifiers_Encapsulation_Readonly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region test
+            /*            User pass = new User();
+                        typeof(User).GetField("_password", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(pass, "dhjakshdkahkd");
+                        var _password  = typeof(User).GetField("_password", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(pass);
+
+                        Console.WriteLine(_password);
+            */
+            #endregion
+            string username = GetInputStr("Enter your username: ", 5, 15);
+
+
+            int userage = GetInputInt("Enter your age: ", 1, 120);
+
+
+            string userpass = PassCheck("Enter your password: ", 8, 15);
+            User user1 = new User()
+            {
+                Username = username,
+                Age = userage,
+                Password = userpass
+
+            };
+
+            user1.UserInfo();
+
+
+
+
+        }
+        static string PassCheck(string name, int min, int max)
+        {
+            string input;
+            int k = 0;
+            int k1 = 0;
+            int k2 = 0;
+
+            do
+            {
+
+                Console.WriteLine(name);
+                input = Console.ReadLine();
+                char[] passw = input.ToCharArray();
+                for (int i = 0; i < passw.Length; i++)
+                {
+                    if (Char.IsNumber(passw[i]) == true)
+                    {
+                        k1 = 1;
+                    }
+                    if (char.IsUpper(passw[i]) == true)
+                    {
+                        k2 = 1;
+                    }
+                }
+
+                k = k1 + k2;
+                return k;
+            }
+
+            while (input.Length < min && input.Length > max && k != 2);
+            return input;
+        }
+
+        static String GetInputStr(string name, int min, int max)
+        {
+            string input;
+            do
+            {
+                Console.Write(name);
+                input = Console.ReadLine();
+
+            } while (input.Length < min || input.Length > max);
+            return input;
+        }
+
+        static int GetInputInt(string name, int min = int.MinValue, int max = int.MaxValue)
+        {
+            int input;
+            do
+            {
+                Console.Write(name);
+                input = Convert.ToInt32(Console.ReadLine());
+
+            } while (input < min || input > max);
+            return input;
+        }
+
+    }
+}
+
